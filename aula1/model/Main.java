4806028c7178e420d7c77ea3dd480e425b627a3b
Gustavo Frainer <guga.frainer1@gmail.com>
2025-08-22 11:59:03 +0000

    aula1 - commit6

--- aula1/model/Main.java
+++ aula1/model/Main.java
@@ -1,9 +1,25 @@
 import model.Assento;
+import model.AssentoEconomica;
+import model.AssentoExecutivo;
+import model.AssentoPrimeiraClasse;
+import model.Passageiro;
 
 public class Main {
     public static void main(String[] args) {
         Assento a1 = new AssentoEconomica("Economica", 50.0);
+        System.out.println("Valor do Assento: " + a1.calcularPrecoFinal());
 
         Assento a2 = new AssentoExecutiva("Executiva", 150.0);
+        System.out.println("Valor do Assento: " + a2.calcularPrecoFinal());
+
+        Passageiro p1 = new Passageiro("Gustavo");
+        System.out.println("Comprando assento na classe economica");
+        p1.setAssento(a1);
+
+        System.out.println("O Assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]");
+
+        System.out.println("realizando o upgrade");
+        p1.setAssento(a2);
+        System.out.println("O Assento do Passageiro ["+p1.getNome()+"] custa [R$"+p1.getAssento().calcularPrecoFinal()+"]");
     }
 }
