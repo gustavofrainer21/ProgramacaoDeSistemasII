7f7d49df2b09ec46cf58c7bc8a41c139eafb84a0
Gustavo Frainer <guga.frainer1@gmail.com>
2025-10-01 16:53:20 +0000

    Commit 1

---
+++ Lab_08/hotelapp/src/main/java/br/mack/ps2/hotelapp/HotelappApplication.java
@@ -0,0 +1,13 @@
+package br.mack.ps2.hotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelappApplication.class, args);
+	}
+
+}
---
+++ Lab_08/hotelapp/src/test/java/br/mack/ps2/hotelapp/HotelappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mack.ps2.hotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
