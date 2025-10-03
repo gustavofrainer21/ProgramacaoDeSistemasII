9a28a5bade912579a0a9c68b1bc7b1408fb4eb15
Gustavo Frainer <guga.frainer1@gmail.com>
2025-10-03 00:42:00 +0000

    Commit 8

--- Lab_08/hotelapp/src/main/java/br/mack/ps2/hotelapp/HotelappApplication.java
+++ Lab_08/hotelapp/src/main/java/br/mack/ps2/hotelapp/HotelappApplication.java
@@ -1,7 +1,11 @@
 package br.mack.ps2.hotelapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.mack.ps2.hotelapp.model.Hospede;
+import br.mack.ps2.hotelapp.dao.DAOHospede;
 
 @SpringBootApplication
 public class HotelappApplication {
