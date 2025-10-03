e21b3ff63d0b5e664124c7cc41080e62b9803245
Gustavo Frainer <guga.frainer1@gmail.com>
2025-10-03 11:36:13 +0000

    Commit 13

--- Lab_08/hotelapp/src/main/java/br/mack/ps2/hotelapp/HotelappApplication.java
+++ Lab_08/hotelapp/src/main/java/br/mack/ps2/hotelapp/HotelappApplication.java
@@ -16,22 +16,34 @@ public class HotelappApplication {
 
 	@Autowired
 	private DAOHospede hospDAO;
+	private DAOReserva resDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelappApplication.class, args);
 	}
 
+	// Método Listar
 	public void listar() {
 		Iterable<Hospede> hospedes = hospDAO.findAll();
+		Iterable<Reserva> reservas = resDAO.findAll();
+		System.out.println("Hóspedes:");
 		for(Hospede h : hospedes) {
 			System.out.println("ID: " + h.getId());
 			System.out.println("Nome: " + h.getNome());
 			System.out.println("Telefone: " + h.getTelefone());
+			System.out.println("-----------------------\n");
+		}
+		System.out.println("Reservas:");
+		for(Reserva r : reservas) {
+			System.out.println("ID: " + r.getId());
+			System.out.println("ID do Hóspede: " + r.getHospede_id());
+			System.out.println("Data de Check-in: " + r.getData_checkin());
 			System.out.println("-----------------------");
 		}
 	}
 
-	public void buscar() {
+	// Método Buscar
+	public void buscarHospede() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o nome do hóspede: ");
 		String nome = sc.nextLine();
@@ -43,8 +55,21 @@ public class HotelappApplication {
 			System.out.println("-----------------------");
 		}
 	}
+	public void buscarReserva() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o ID do hóspede: ");
+		int hospede_id = sc.nextInt();
+		List<Reserva> reservas = resDAO.findByHospede_id(hospede_id);
+		for(Reserva r : reservas) {
+			System.out.println("ID: " + r.getId());
+			System.out.println("ID do Hóspede: " + r.getHospede_id());
+			System.out.println("Data de Check-in: " + r.getData_checkin());
+			System.out.println("-----------------------");
+		}
+	}
 
-	public void criar() {
+	// Método Criar
+	public void criarHospede() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o nome do hóspede: ");
 		String nome = sc.nextLine();
@@ -56,8 +81,22 @@ public class HotelappApplication {
 		hospDAO.save(h);
 		System.out.println("Hóspede criado com sucesso!");
 	}
+	public void criarReserva() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o ID do hóspede: ");
+		int hospede_id = sc.nextInt();
+		sc.nextLine(); // Consumir a quebra de linha
+		System.out.print("Digite a data de check-in (formato YYYY-MM-DD): ");
+		String data_checkin = sc.nextLine();
+		Reserva r = new Reserva();
+		r.setHospede_id(hospede_id);
+		r.setData_checkin(data_checkin);
+		resDAO.save(r);
+		System.out.println("Reserva criada com sucesso!");
+	}
 
-	public void alterarDados() {
+	// Método Alterar
+	public void alterarDadosHospede() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o ID do hóspede: ");
 		Long id = sc.nextLong();
@@ -77,8 +116,30 @@ public class HotelappApplication {
 			System.out.println("Hóspede não encontrado!");
 		}
 	}
+	public void alterarDadosReserva() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o ID da reserva: ");
+		Long id = sc.nextLong();
+		sc.nextLine(); // Consumir a quebra de linha
+		Optional<Reserva> optionalReserva = resDAO.findById(id);
+		if(optionalReserva.isPresent()) {
+			Reserva r = optionalReserva.get();
+			System.out.print("Digite o novo ID do hóspede (atual: " + r.getHospede_id() + "): ");
+			int hospede_id = sc.nextInt();
+			sc.nextLine(); // Consumir a quebra de linha
+			System.out.print("Digite a nova data de check-in (atual: " + r.getData_checkin() + "): ");
+			String data_checkin = sc.nextLine();
+			r.setHospede_id(hospede_id);
+			r.setData_checkin(data_checkin);
+			resDAO.save(r);
+			System.out.println("Reserva alterada com sucesso!");
+		} else {
+			System.out.println("Reserva não encontrada!");
+		}
+	}
 
-	public void deletar() {
+	// Método Deletar
+	public void deletarHospede() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o ID do hóspede: ");
 		Long id = sc.nextLong();
@@ -90,20 +151,33 @@ public class HotelappApplication {
 			System.out.println("Hóspede não encontrado!");
 		}
 	}
+	public void deletarReserva() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o ID da reserva: ");
+		Long id = sc.nextLong();
+		Optional<Reserva> optionalReserva = resDAO.findById(id);
+		if(optionalReserva.isPresent()) {
+			resDAO.deleteById(id);
+			System.out.println("Reserva deletada com sucesso!");
+		} else {
+			System.out.println("Reserva não encontrada!");
+		}
+	}
 
 	public void run(String... args) throws Exception {
 		System.out.println("===Menu de Opções===");
 		System.out.println("1 - Listar Todos os Hóspedes e Reservas");
 		System.out.println("2 - Buscar Hóspede por Nome");
-		//System.out.println("3 - Buscar Reserva por ID do Hóspede");
-		System.out.println("3 - Criar novo Hóspede");
-		//System.out.println("  - Criar nova Reserva");
-		System.out.println("4 - Alterar dados de um Hóspede");
-		//System.out.println("  - Alterar dados de uma Reserva");
-		System.out.println("5 - Deletar um Hóspede por ID");
-		//System.out.println("  - Deletar uma Reserva por ID");
+		System.out.println("3 - Buscar Reserva por ID do Hóspede");
+		System.out.println("4 - Criar novo Hóspede");
+		System.out.println("5 - Criar nova Reserva");
+		System.out.println("6 - Alterar dados de um Hóspede");
+		System.out.println("7 - Alterar dados de uma Reserva");
+		System.out.println("8 - Deletar um Hóspede por ID");
+		System.out.println("9 - Deletar uma Reserva por ID");
 		System.out.println("0 - Sair");
 		System.out.println();
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite a opção desejada: ");
 		int opcao = sc.nextInt();
@@ -114,20 +188,36 @@ public class HotelappApplication {
 				listar();
 				break;
 			case 2:
-				System.out.println("Buscando por Nome:");
-				buscar();
+				System.out.println("Buscando Hóspede por Nome:");
+				buscarHospede();
 				break;
 			case 3:
-				System.out.println("Criando novo Hóspede:");
-				criar();
+				System.out.println("Buscando Reserva por ID do Hóspede:");
+				buscarReserva();
 				break;
 			case 4:
-				System.out.println("Alterando dados de um Hóspede:");
-				alterarDados();
+				System.out.println("Criando novo Hóspede:");
+				criarHospede();
 				break;
 			case 5:
+				System.out.println("Criando nova Reserva:");
+				criarReserva();
+				break;
+			case 6:
+				System.out.println("Alterando dados de um Hóspede:");
+				alterarDadosHospede();
+				break;
+			case 7:
+				System.out.println("Alterando dados de uma Reserva:");
+				alterarDadosReserva();
+				break;
+			case 8:
 				System.out.println("Deletando um Hóspede por ID:");
-				deletar();
+				deletarHospede();
+				break;
+			case 9:
+				System.out.println("Deletando uma Reserva por ID:");
+				deletarReserva();
 				break;
 			case 0:
 				System.out.println("Saindo...");
